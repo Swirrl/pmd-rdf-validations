cf3a04ca4fbbb848d5f73a3c7b794819a99a5b3e
Kieran Livesey <kieran@swirrl.com>
2021-09-27 16:08:15 +0100

    Test PMD4 suite for validations that have been removed

--- test/pmd4_test.clj
+++ test/pmd4_test.clj
@@ -1,6 +1,6 @@
 (ns pmd4-test
-  (:require [clojure.test :refer [deftest]]
-            [test-validation :refer [test-validation]]
+  (:require [clojure.test :refer [deftest is]]
+            [test-validation :refer [test-validation run-validation-suite]]
             [rdf-validator.endpoint :as ep]
             [rdf-validator.core :as rv])
   (:import [java.net URI]))
@@ -23,4 +23,12 @@
     "pmd4/src/swirrl/validations/pmd4/SELECT_DatasetMinOneGraph.sparql"
     "test/pmd4/swirrl/validations/pmd4/SELECT_DatasetMinOneGraph.ttl"
     #{{:dataset (URI. "http://test/graph/0#fail")}}))
+
+; Run the whole suite against removed validations.
+(deftest remove-dataset-exactly-one-label
+  (let [[summary results]
+        (run-validation-suite "pmd4/src/rdf-validator-suite.edn"
+                              "test/pmd4/swirrl/validations/pmd4/removed.ttl")]
+    (is (= [] (vec (filter #(not= :passed (:result %)) results))))
+    (is (= {:passed 34 :failed 0 :errored 0 :ignored 0} summary))))
 
--- test/test_validation.clj
+++ test/test_validation.clj
@@ -1,9 +1,13 @@
 (ns test-validation
   (:require [clojure.test :refer [is]]
+            [clojure.java.io :as io]
             [rdf-validator.endpoint :as ep]
+            [rdf-validator.test-cases :as tc]
             [rdf-validator.core :as rv]))
 
 (defn test-validation
+  "Test the sparql query in query-file returns the expected results when
+  run on the data in ttl-data-file."
   [query-file ttl-data-file expected]
   (let [test-case {:source query-file}
         repository (ep/parse-repository ttl-data-file)
@@ -11,4 +15,20 @@
     (is (= expected
            (set (:errors (rv/run-test-case test-case {} endpoint)))))))
 
+(defn run-validation-suite
+  "Run the validation in suite-source-file (e.g. rdf-validator-suite.edn)
+  against the data in ttl-data-file.
+  Returns [summary test-results]."
+  [suite-source-file ttl-data-file]
+  (let [suite-source (io/file suite-source-file)
+        repository (ep/parse-repository ttl-data-file)
+        endpoint (ep/create-endpoint repository [])
+        suites (tc/resolve-test-suites [suite-source])
+        test-cases (tc/suite-tests suites [])
+        test-results (rv/run-test-cases test-cases {} endpoint)
+        summary (reduce (fn [summary {:keys [result]}]
+                          (update summary result inc))
+                        {:failed 0 :passed 0 :errored 0 :ignored 0}
+                        test-results)]
+    [summary test-results]))
 
